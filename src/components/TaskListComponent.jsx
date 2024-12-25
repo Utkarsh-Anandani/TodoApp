374361e580bbc94f2dcc634e69244ad3acc93188
Utkarsh-Anandani <utkarshanandani9@gmail.com>
2024-12-25 19:05:36 +0530

    Added clear all functionality for each date

--- src/components/TaskListComponent.jsx
+++ src/components/TaskListComponent.jsx
@@ -21,15 +21,30 @@ const TodoList = () => {
     return groups;
   }, {});
 
+  const clearAll = (date) => {
+    if(confirm("Are you sure, all task for the date will be deleted!")){
+      setTodos((prev) => prev.filter((todo) => todo.date !== date));
+    } else {
+      
+    }
+  }
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14'>
       {Object.entries(groupedTodos).map(([date, tasks]) => (
         <div key={date} className="mb-4">
           <div className={`flex justify-between items-center bg-gray-200 p-2 ${darkMode && 'bg-gray-700'}`}>
             <h2 className="font-bold">{date}</h2>
+            <div className='flex gap-5'>
+            <button onClick={() => clearAll(date)}
+              className='bg-red-700 text-white font-semibold px-2 py-1 rounded text-sm'
+            >
+              Clear All
+            </button>
             <button onClick={() => toggleExpand(date)}>
               {expandedDates[date] ? <IoIosArrowUp /> : <IoIosArrowDown />}
             </button>
+            </div>
           </div>
           {expandedDates[date] && (
             <div className={`p-2 border ${darkMode && 'border-gray-600'}`}>
@@ -42,13 +57,13 @@ const TodoList = () => {
                   <div className="flex gap-3">
                     <button
                       onClick={() => deleteTodo(task.id)}
-                      className="bg-red-500 text-gray-200 rounded-md px-3 py-2 my-auto text-lg"
+                      className="bg-red-700 text-gray-50 rounded-md px-3 py-2 my-auto text-lg"
                     >
                       <MdDelete />
                     </button>
                     <button
                       onClick={() => updateTodo(task.id)}
-                      className="bg-black text-gray-200 rounded-md px-3 py-2 my-auto text-lg"
+                      className="bg-black text-gray-50 rounded-md px-3 py-2 my-auto text-lg"
                     >
                       <FaEdit />
                     </button>
